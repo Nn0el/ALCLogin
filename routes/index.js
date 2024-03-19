e7584a742be83bfbdaa098075476301ff56e1b63
Mille Tedebring Thysell <milletedebring.thysell@elev.ga.ntig.se>
2024-03-19 10:09:48 +0100

    hashat lösenord

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,46 @@ const router = express.Router()
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
+const HASHFRÅNDB = 'BLABLA'
 
 router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome' })
 })
 
 router.get('/login', function (req, res) {
-  res.render('login.njk', { title: 'Welcome' })
+  bcrypt.hash("test", 10, function (err, hash) {
+    bcrypt.compare(myPlaintextPassword, HASHFRÅNDB, function(err, result){
+     
+    });
+     
+     console.log(hash);
+     return res.json(hash);
+   });
+})
+
+router.post('/login', async function (req,res){
+  const user = req.body.user
+  const password = req.body.password
+  console.log(user, password)
+  const [results] = await pool.promise().query('INSERT INTO patch_login (user, password) VALUES (?, ?)',[user, password])
+  res.json(req.body)
 })
 
 router.get('/secret', function (req, res) {
   res.render('secret.njk', { title: 'Welcome' })
 })
 
-router.get('/hash', async function (req, res) 
-{
+router.get('/hash', async function (req, res) {
 
-bcrypt.hash("test", 10, function(err, hash){
-  bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
-    // result == true
-});
-bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-    // result == false
-});
-  console.log(hash);
-  return res.json(hash);
-});
+  bcrypt.hash("test", 10, function (err, hash) {
+   bcrypt.compare(myPlaintextPassword, HASHFRÅNDB, function(err, result){
+    
+   });
+    
+    console.log(hash);
+    return res.json(hash);
+  });
 
-
-  res.render('hash.njk', { title: 'Welcome' })
 })
 
 router.get('/dbtest', async function (req, res) {
